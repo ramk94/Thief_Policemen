976548b08cec57f010a11669e0d0a8ea827aa07b
shengwubin <shengwubin@gmail.com>
2019-05-20 16:26:23 -0700

    update camera_system interfaces

--- src/camera_system.py
+++ src/camera_system.py
@@ -1,5 +1,10 @@
+import numpy as np
+
+
 def get_image():
     """
     input: None
     output: image: a three-dimensional array which indicates an image.(I am not sure about the indexing conventions because different libraries may be different when indexing)
     """
+    image = np.random((1920, 1080, 3))
+    return image
