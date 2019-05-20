2243eea370ed75a932d075cd29b68a6f75c80f04
shengwubin <shengwubin@gmail.com>
2019-05-20 16:33:26 -0700

    update strategy module interfaces

--- src/strategy.py
+++ src/strategy.py
@@ -5,4 +5,9 @@ class Strategy:
     def get_next_steps(self, graph, objects_on_graph):
         """
         """
-        
+        instructions = {
+            'thief': (3, 4),
+            'policeman1': (7, 6),
+            'policeman2': (9, 8)
+        }
+        return instructions
