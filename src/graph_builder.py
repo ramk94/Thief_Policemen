3c74d9106b798d857bca2eb90f53161bc7bed176
wubin <shengwubin@gmail.com>
2019-05-24 15:32:12 -0700

    add a graph builder output example

--- src/graph_builder.py
+++ src/graph_builder.py
@@ -30,14 +30,49 @@ class GraphBuilder:
         ----------
         graph: numpy array
             N * N matrix which describes a graph
+            example:
+                Assume we have nine nodes on the gaming board, and we already know the center coordinate of each node.
+                Then an output graph(numpy array) format may like the array below, and you can find the image of this
+                example in Thief_Policemen/resources/examples/gaming_board_graph.JPG
+                [  # 1 2 3 4 5 6 7 8 9
+                    [0,0,1,0,0,0,0,0,0],
+                    [0,0,1,0,0,1,0,0,0],
+                    [1,1,0,1,0,0,0,0,0],
+                    [0,0,1,0,0,0,0,1,0],
+                    [0,0,0,0,0,1,0,0,0],
+                    [0,1,0,0,1,0,1,0,0],
+                    [0,0,0,0,0,1,0,1,0],
+                    [0,0,0,1,0,0,1,0,1],
+                    [0,0,0,0,0,0,0,1,0]
+                ]
         objects_on_graph: dict
             a dict which indicates robots' locations on the graph
+            example:
+                Assume thief is at node #3, policeman1 is at node 7 and policeman2 is at node 9, then this dict may like
+                the dict below.
+                {
+            "thief:: 3,
+            "policeman1": 7,
+            "policeman2": 9
+        }
         """
-        num_nodes = len(self.centers)
-        graph = np.zeros((num_nodes, num_nodes), dtype=np.int64)
+        example = [
+            [0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 1, 0, 0, 0],
+            [1, 1, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0],
+            [0, 1, 0, 0, 1, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 1, 0],
+            [0, 0, 0, 1, 0, 0, 1, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0, 1, 0]
+        ]
+        graph = np.array(example, dtype=np.int64)
         objects_on_graph = {
             'thief': 3,
             'policeman1': 7,
             'policeman2': 9
         }
+        num_nodes = len(self.centers)
+        assert num_nodes == graph.shape[0] and num_nodes == graph.shape[1]
         return graph, objects_on_graph
