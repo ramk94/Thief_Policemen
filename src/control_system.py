e68dbb1cbe15ad6cc9ec5aaaf0b493ae583afae6
shengwubin <shengwubin@gmail.com>
2019-05-31 20:07:23 -0700

    improve control system

--- src/control_system.py
+++ src/control_system.py
@@ -1,6 +1,7 @@
 import numpy as np
 import logging
 from robot_client import Robot
+
 logger = logging.getLogger(__name__)
 
 
@@ -9,7 +10,7 @@ class Controller:
     Control system is the most important part of our project.
     """
 
-    def __init__(self):
+    def __init__(self, robots=None):
         self.robot_client = Robot('thief', '192.168.1.106', 4242)
         self.last_vector = None
 
@@ -25,6 +26,8 @@ class Controller:
             objects' relative locations, relative sizes and categories
         instructions: dict
             a dict consists of future decisions
+        sensor_data: dict
+            a dict consists of orientation data(only use sensor when sensor_data is None)
 
         Returns
         -------
@@ -39,41 +42,35 @@ class Controller:
             orientation = sensor_data[key]['orientation']
 
             # build orientation unit vectors
-            current_direction = np.array(
-                orientation['current'][0:2]).reshape((-1, 1))
-            current_direction = current_direction / \
-                np.linalg.norm(current_direction)
-            base_unit_vector = np.array(
-                orientation['base'][0:2]).reshape((-1, 1))
-            base_unit_vector = base_unit_vector / \
-                np.linalg.norm(base_unit_vector)
+            current_direction = np.array(orientation['current']).reshape((-1, 1))
+            current_direction_unit = current_direction / np.linalg.norm(current_direction)
+            base_direction = np.array(orientation['base']).reshape((-1, 1))
+            base_direction_unit = base_direction / np.linalg.norm(base_direction)
 
             # build center vectors
             current_center = np.array(value['center']).reshape((-1, 1))
             next_center = np.array(
-                centers[instructions[key][1]-1]).reshape((-1, 1))
+                centers[instructions[key][1] - 1]).reshape((-1, 1))
 
             # calculate the angle between centers
             delta = next_center - current_center
-            dot = np.dot(base_unit_vector.T, delta)
-            det = base_unit_vector[0] * delta[1] - \
-                base_unit_vector[1] * delta[0]
-            theta = np.arctan2(det, dot)
+            dot = np.dot(base_direction_unit.T, delta)
+            det = np.cross(delta.flatten(), base_direction_unit.flatten())
+            theta = np.arctan2(det, dot) * 180 / np.pi
 
             # calculate the angle between orientations
-            dot = np.dot(base_unit_vector.T, current_direction)
-            det = base_unit_vector[0] * current_direction[1] - \
-                base_unit_vector[1] * current_direction[0]
-            alpha = np.arctan2(det, dot)
+            dot = np.dot(base_direction_unit.T, current_direction_unit)
+            det = np.cross(current_direction_unit.flatten(), base_direction_unit.flatten())
+            alpha = np.arctan2(det, dot) * 180 / np.pi
 
             # calculate rotate angle
-            gamma = (theta - alpha) * 180 / np.pi
+            gamma = theta - alpha
 
             # construct rotate signal
             signals.append({
                 'name': key,
                 'type': 'rotate',
-                'param': gamma.item()
+                'param': int(gamma.item())
             })
 
             # construct move signal
@@ -83,12 +80,6 @@ class Controller:
                 'param': 1
             })
         return signals
-
-    def get_orientation(self):
-        self.robot_client.move_forward(4)
-        self.robot_client.rotate(180)
-        self.robot_client.move_forward(4)
-        self.robot_client.rotate(180)
 
     def get_sensor_data(self):
         """
@@ -102,20 +93,20 @@ class Controller:
                 sensor_data = {
                     'thief': {
                         'orientation': {
-                            'base': (0, 1, 3),
-                            'current': (1, 0, 1.5)
+                            'base': (0, 1),
+                            'current': (1, 0)
                         }
                     },
                     'policeman1': {
                         'orientation': {
-                            'base': (0, 1, 3),
-                            'current': (1, 0, 1.5)
+                            'base': (0, 1),
+                            'current': (1, 0)
                         }
                     },
                     'policeman2': {
                         'orientation': {
-                            'base': (0, 1, 3),
-                            'current': (1, 0, 1.5)
+                            'base': (0, 1),
+                            'current': (1, 0)
                         }
                     }
                 }
@@ -123,20 +114,20 @@ class Controller:
         sensor_data = {
             'thief': {
                 'orientation': {
-                    'base': (0, 1, 3),
-                    'current': (1, 0, 1.5)
+                    'base': (0, 1),
+                    'current': (1, 0)
                 }
             },
             'policeman1': {
                 'orientation': {
-                    'base': (0, 1, 3),
-                    'current': (1, 0, 1.5)
+                    'base': (0, 1),
+                    'current': (1, 0)
                 }
             },
             'policeman2': {
                 'orientation': {
-                    'base': (0, 1, 3),
-                    'current': (1, 0, 1.5)
+                    'base': (0, 1),
+                    'current': (1, 0)
                 }
             }
         }
@@ -151,6 +142,8 @@ class Controller:
         control_signals: dict
             control signals based on instructions
         """
+        for signal in control_signals:
+            pass
 
     def is_finished(self, centers, object_list, instructions, threshold=0.05):
         """
@@ -174,9 +167,9 @@ class Controller:
         for key, value in object_list.items():
             current_center = np.array(value['center'])
             target = instructions[key][1]
-            target_center = np.array(centers[target-1])
-            print("error: "+str(np.linalg.norm(current_center-target_center)))
-            if np.linalg.norm(current_center-target_center) <= threshold:
+            target_center = np.array(centers[target - 1])
+            print("error: " + str(np.linalg.norm(current_center - target_center)))
+            if np.linalg.norm(current_center - target_center) <= threshold:
                 logger.info('{0} has moved to node {1}.'.format(key, target))
             else:
                 is_done = False
@@ -197,10 +190,10 @@ if __name__ == '__main__':
 
     window_name = 'test'
     cv2.namedWindow(window_name)
-    # wait for exit flag
+
     object_list = []
     while len(object_list) == 0:
-        image = get_image(save=True)
+        image = get_image(save=False)
         object_list = detector.detect_objects(image)
     print(object_list)
 
@@ -210,7 +203,7 @@ if __name__ == '__main__':
         for key, value in object_list.items():
             height, width = image.shape[0], image.shape[1]
             cv2.circle(
-                image, (int(value['center'][0]*width), int(value['center'][1]*height)), 10, (255, 0, 0), -1)
+                image, (int(value['center'][0] * width), int(value['center'][1] * height)), 10, (255, 0, 0), -1)
     cv2.imshow(window_name, image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
@@ -220,28 +213,28 @@ if __name__ == '__main__':
     ]
     controller = Controller()
     controller.robot_client.move_forward(4)
-    # controller.robot_client.rotate(180)
 
     object_list = []
     while len(object_list) == 0:
-        image = get_image(save=True)
+        image = get_image(save=False)
         object_list = detector.detect_objects(image)
     p2 = np.array(object_list['thief']['center']).reshape((-1, 1))
 
-    sensor_data = {
+    sensors = {
         'thief': {
             'orientation': {
-                'base': (0, 1, 3),
-                'current': ((p2-p1)[0], (p2-p1)[1], 1.5)
+                'base': (0, 1),
+                'current': ((p2 - p1)[0], (p2 - p1)[1])
             }
         }
     }
+
     target_vector = np.array(centers[1]).reshape((-1, 1))
-    controller.last_vector = target_vector-p2
+    controller.last_vector = target_vector - p2
 
     signals = controller.calculate_control_signals(
-        centers, object_list, {'thief': (1, 2)}, sensor_data)
-    alpha = int(signals[0]['param'])+20
+        centers, object_list, {'thief': (1, 2)}, sensors)
+    alpha = int(signals[0]['param'])
     print('alpha is {}'.format(alpha))
     controller.robot_client.rotate(alpha)
 
@@ -249,46 +242,58 @@ if __name__ == '__main__':
 
     while not controller.is_finished(centers, object_list, {'thief': (1, 2)}):
         target_vector = np.array(centers[1]).reshape((-1, 1))
+
         controller.robot_client.move_forward(2)
+
         object_list = []
         while len(object_list) == 0:
-            image = get_image(save=True)
+            image = get_image(save=False)
             object_list = detector.detect_objects(image)
         print(object_list)
         vector_current = np.array(
             object_list['thief']['center']).reshape((-1, 1))
-        sensor_data = {
+        sensors = {
             'thief': {
                 'orientation': {
-                    'base': (0, 1, 3),
-                    'current': ((vector_current-vector_previous)[0], (vector_current-vector_previous)[1], 1.5)
+                    'base': (0, 1),
+                    'current': ((vector_current - vector_previous)[0], (vector_current - vector_previous)[1])
                 }
             }
         }
-
-        def unit_vector(vector):
-            return vector/np.linalg.norm(vector)
-
-        def angle_between(v1, v2):
-            v1_u = unit_vector(v1)
-            v2_u = unit_vector(v2)
-            return np.arccos(np.dot(v1_u.T, v2_u))/np.pi*180
-        rotate = False
-        current_direction = vector_current-vector_previous
-        controller.last_vector = target_vector-vector_current
-        alpha = angle_between(current_direction, controller.last_vector).item()
-        alpha = int(alpha)
-        if alpha > 30:
-            rotate = True
+        signals = controller.calculate_control_signals(
+            centers, object_list, {'thief': (1, 2)}, sensors)
+        alpha = int(signals[0]['param'])
         print('alpha is {}'.format(alpha))
-        rotate = False
-        if rotate:
+        if alpha > 20:
             controller.robot_client.rotate(alpha)
+        #
+        # def unit_vector(vector):
+        #     return vector / np.linalg.norm(vector)
+        #
+        #
+        # def angle_between(v1, v2):
+        #     v1_u = unit_vector(v1)
+        #     v2_u = unit_vector(v2)
+        #     return np.arccos(np.dot(v1_u.T, v2_u)) / np.pi * 180
+        #
+        #
+        # rotate = False
+        # current_direction = vector_current - vector_previous
+        # controller.last_vector = target_vector - vector_current
+        # alpha = angle_between(current_direction, controller.last_vector).item()
+        # alpha = int(alpha)
+        # if alpha > 30:
+        #     rotate = True
+        # print('alpha is {}'.format(alpha))
+        # rotate = False
+        # if rotate:
+        #     controller.robot_client.rotate(alpha)
         if len(object_list) > 0:
             for key, value in object_list.items():
                 height, width = image.shape[0], image.shape[1]
-                cv2.circle(
-                    image, (int(value['center'][0]*width), int(value['center'][1]*height)), 10, (255, 0, 0), -1)
+                x = int(value['center'][0] * width)
+                y = int(value['center'][1] * height)
+                cv2.circle(image, (x, y), 10, (255, 0, 0), -1)
         cv2.imshow(window_name, image)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
